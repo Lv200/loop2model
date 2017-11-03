e41b70ed0bb693c01320a8b6c99c37280b5ad8c8
Iscream <2557384174@qq.com>
2017-11-03 20:29:57 +0800

    obtain the input from csv file

--- rvnn.py
+++ rvnn.py
@@ -1,6 +1,10 @@
-import numpy as np
-import tensorflow as tf
+# -*- coding: utf-8 -*-
 
+import tensorflow as tf
+from pandas import DataFrame, Series
+import numpy as np
+import pandas as pd
+from util import *
 
 class Config(object):
 	lr = 0.01
@@ -14,19 +18,29 @@ class RvNN_Model():
 
 	def load_data(self):
 		"""load train/test dataset"""
-
+		loops = pd.read_csv('./dataset/train_loop.csv', header = 0, low_memory = False)
+		loops.columns = ['loop_infos', 'sym_shapes', 'tree_kids', 'sym_params']		
+		loop_infos = dtypeTransform(loops['loop_infos'], float)
+		sym_shapes = dtypeTransform(loops['sym_shapes'], float)
+		tree_kids = dtypeTransform(loops['tree_kids'], int)
+		sym_params = dtypeTransform(loops['sym_params'], float)
+		#print sym_params
 
 	def inference(self, tree):
 		"""For a given tree build the RvNN models computation graph up to where it may be used for inference"""
+		pass
 
 
 	def add_model_vars(self):
+		pass
 
 
 	def add_model(self):
+		pass
 
 
 	def loss(self, logits, labels):
+		pass
 
 
 	def training(self, loss):
@@ -42,6 +56,7 @@ class RvNN_Model():
 		return predictions
 
 	def predict(self):
+		pass
 
 	
 	def run_epoch(self):
@@ -49,6 +64,7 @@ class RvNN_Model():
 
 	
 	def train(self):
+		pass
 		
 
 def test_RvNN():
@@ -56,6 +72,6 @@ def test_RvNN():
 	model = RvNN_Model(config)
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 	test_RvNN()
 		
